a5f5912b16fa6d0af278208ad74f9f98f1959455
Filippe <lippeoliveira89@gmail.com>
2019-05-30 00:21:50 -0300

    Added animations for containers

--- lib/widgets/login_widgets/animated_login_one.dart
+++ lib/widgets/login_widgets/animated_login_one.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class AnimatedLoginOne extends StatefulWidget {
@@ -5,44 +7,100 @@ class AnimatedLoginOne extends StatefulWidget {
   _AnimatedLoginOneState createState() => _AnimatedLoginOneState();
 }
 
-class _AnimatedLoginOneState extends State<AnimatedLoginOne> {
+class _AnimatedLoginOneState extends State<AnimatedLoginOne>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  AnimationController _formAnimationController;
+  Animation<double> _containerScaleHeight;
+  Animation<double> _containerTransitionMiddle;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this, // the SingleTickerProviderStateMixin
+      duration: Duration(milliseconds: 500),
+    );
+    ;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _containerScaleHeight =
+        Tween<double>(begin: 100.0, end: MediaQuery.of(context).size.height)
+            .animate(_controller);
+    _containerTransitionMiddle = Tween<double>(
+            begin: 50, end: (MediaQuery.of(context).size.height / 2) - 50)
+        .animate(_controller);
+
     return Container(
       color: Colors.white,
       child: Column(
         children: <Widget>[
           Stack(
             children: <Widget>[
-              Container(
-                color: Colors.blue,
-                height: 100.0,
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  return Container(
+                    color: Colors.blue,
+                    height: _containerScaleHeight.value,
+                  );
+                },
               ),
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: Container(
-                    color: Colors.pink,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ),
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  return Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: _containerTransitionMiddle.value),
+                      child: Container(
+                        color: Colors.pink,
+                        height: 100.0,
+                        width: 100.0,
+                      ),
+                    ),
+                  );
+                },
               )
             ],
           ),
           new Expanded(
             child: Center(
-              child: MaterialButton(
-                //elevation: 10,
-                color: Colors.blueAccent,
-                child: Text(
-                  "Execute",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.white),
+              child: FadeTransition(
+                opacity: Tween(begin: 1.0, end: 0.0).animate(_controller),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    MaterialButton(
+                    //elevation: 10,
+                    color: Colors.blueAccent,
+                    child: Text(
+                      "Execute",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _controller.forward();
+
+                      Timer(new Duration(seconds: 5), () {
+                        _controller.reverse();
+                      });
+                    })
+                  ],
                 ),
-                onPressed: () {},
               ),
             ),
           )
